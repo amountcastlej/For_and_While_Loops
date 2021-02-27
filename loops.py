c2789504fdec90f9c874a5d533f031fa1dd8f6af
Adam Joines <amountcastlej@gmail.com>
2021-02-27 00:05:37 -0500

    Python file added

---
+++ loops.py
@@ -0,0 +1,80 @@
+# For Loops
+
+#for x in range(0, 10,1 ):
+#    print(x)
+
+#for x in range(0, 10):
+#    print(x)
+
+#for x in range(10):
+#    print(x)
+
+#for x in range(0, 10, 2):
+#    print(x)
+
+#for x in range(5, 1, -3):
+#    print(x)
+
+#my_list = ["abc", 123, "xyz"]
+#for i in range(0, len(my_list)):
+#    print(i, my_list[i])
+
+#my_dict = {"name": "Noelle", "language": "Python"}
+#for k in my_dict:
+#    print(k)
+
+#my_dict = {"name": "Noelle", "language": "Python"}
+#for k in my_dict:
+#    print(my_dict[k])  
+
+#Alternative way to find keys
+#for key in capitals.keys():
+    #print(key)
+
+#Alternative way to find values
+#for val in capitals.value():
+    #print(val)
+
+#To find both keys and values
+#for key, val in my_dict.items():
+#    print(key, " = ", val)
+
+#for count in range(0,5):
+#    print("Looping - ", count)
+
+#for val in "string":
+#    if val == "i":
+#        break
+#    print(val)
+
+#for val in "string":
+#    if val == "i":
+#        continue
+#    print(val)
+
+# While Loops
+
+#count = 0
+#while count < 5:
+#    print("looping - ", count)
+#    count += 1
+
+#Syntax of While loop
+#while <expression>:
+    # do something, including progress towards making the expression false, Otherwise we'll never get out of here!
+
+#y = 3
+#while y > 0:
+#    print(y)
+#    y = y - 1
+#else: 
+#    print("Final else statement")
+
+#y = 3
+#while y > 0:
+#    print(y)
+#    y = y - 1
+#    if y == 0:
+#        break
+#else: 
+#    print("final ese statement")
